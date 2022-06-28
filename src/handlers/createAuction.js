ecf60f1ab62d202ffcf5092c2483c79349095750
Jadson Sanches <jadson.sanches@7comm.com.br>
2022-06-28 17:43:13 -0300

    Added http-errors in package.json and add http events in createAuction

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,10 +1,15 @@
 const { v4 } = require('uuid');
 const AWS = require('aws-sdk');
+const { default: middy } = require('@middy/core');
+const httpJsonBodyParser = require('@middy/http-json-body-parser')
+const httpEventNormalizer = require('@middy/http-event-normalizer')
+const httpErrorHandler = require('@middy/http-error-handler')
+const createError = require('http-errors')
 
 const createAuction = async (event) => {
   const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-  const { title } = JSON.parse(event.body);
+  const { title } = event.body;
   const now = new Date();
 
   const auction = {
@@ -14,10 +19,15 @@ const createAuction = async (event) => {
     createAt: now.toISOString(),
   }
 
-  await dynamodb.put({
-    TableName: process.env.AUCTION_TABLE_NAME,
-    Item: auction,
-  }).promise();
+  try {
+    await dynamodb.put({
+      TableName: process.env.AUCTION_TABLE_NAME,
+      Item: auction,
+    }).promise();
+  } catch (error) {
+    console.log(error);
+    throw new createError.InternalServerError(error);
+  }
 
   return {
     statusCode: 201,
@@ -26,5 +36,8 @@ const createAuction = async (event) => {
 };
 
 module.exports = {
-  handler: createAuction,
+  handler: middy(createAuction)
+    .use(httpJsonBodyParser())
+    .use(httpEventNormalizer())
+    .use(httpErrorHandler())
 }
