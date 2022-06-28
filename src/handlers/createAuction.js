77ecb7ecf3c9421b56bde2011fe8a4de33b45923
Jadson Sanches <jadson.sanches@7comm.com.br>
2022-06-28 17:46:34 -0300

    .

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,6 +1,6 @@
 const { v4 } = require('uuid');
 const AWS = require('aws-sdk');
-const { default: middy } = require('@middy/core');
+const middy = require('@middy/core');
 const httpJsonBodyParser = require('@middy/http-json-body-parser')
 const httpEventNormalizer = require('@middy/http-event-normalizer')
 const httpErrorHandler = require('@middy/http-error-handler')
