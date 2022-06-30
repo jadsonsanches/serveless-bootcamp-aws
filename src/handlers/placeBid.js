238a79068f2a58725ae25b1521b555d010ce91c9
Jadson Sanches <jadson.sanches@7comm.com.br>
2022-06-30 14:40:58 -0300

    Create function placeBid to update an Auction

--- src/handlers/placeBid.js
+++ src/handlers/placeBid.js
@@ -4,42 +4,40 @@ const middy = require('@middy/core');
 const httpJsonBodyParser = require('@middy/http-json-body-parser')
 const httpEventNormalizer = require('@middy/http-event-normalizer')
 const httpErrorHandler = require('@middy/http-error-handler')
-const createError = require('http-errors')
+const createError = require('http-errors');
 
-const createAuction = async (event) => {
+const placeBid = async (event) => {
   const dynamodb = new AWS.DynamoDB.DocumentClient();
-
-  const { title } = event.body;
-  const now = new Date();
-
-  const auction = {
-    id: v4(),
-    title,
-    status: 'OPEN',
-    createAt: now.toISOString(),
-    highestBid: {
-      amount: 0
-    }
-  }
+  const { id } = event.pathParameters;
+  const { amount } = event.body;
+  let updateAuction = "";
 
   try {
-    await dynamodb.put({
+    const result = await dynamodb.update({
       TableName: process.env.AUCTION_TABLE_NAME,
-      Item: auction,
+      Key: { id },
+      UpdateExpression: 'set highestBid.amount = :amount',
+      ExpressionAttributeValues: {
+        ':amount': amount,
+      },
+      ReturnValues: 'ALL_NEW'
     }).promise();
+
+    updateAuction = result.Attributes;
+    
   } catch (error) {
     console.error(error);
     throw new createError.InternalServerError(error);
   }
 
   return {
-    statusCode: 201,
-    body: JSON.stringify(auction),
+    statusCode: 200,
+    body: JSON.stringify(updateAuction),
   };
 };
 
 module.exports = {
-  handler: middy(createAuction)
+  handler: middy(placeBid)
     .use(httpJsonBodyParser())
     .use(httpEventNormalizer())
     .use(httpErrorHandler())
